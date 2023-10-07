f84785e5da119a364ed86037984c8602278dc68a
bxcnn <126531264+kkikikikii@users.noreply.github.com>
2023-10-07 15:09:32 +0800

    Update task.py

--- task.py
+++ task.py
@@ -4,7 +4,6 @@ import argparse
 import json
 import requests
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 parser = argparse.ArgumentParser(description='下载任务管理')
 parser.add_argument("--act", help="操作类型：download,del", default="download")
